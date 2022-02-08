460d4410903ee95c8f48c59405d8b81c476b0728
mvila <mvila@voxelgroup.net>
2022-02-08 18:53:21 +0100

    feat: return fizzbuzz when multiple of 3 and 5

--- FizzBuzzKata/FizzBuzzKata/FizzBuzzer.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzzer.cs
@@ -7,6 +7,8 @@
     {
         public static object FizzBuzz(int number)
         {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
             if (number % 3 == 0)
                 return "Fizz";
             if (number % 5 == 0)
